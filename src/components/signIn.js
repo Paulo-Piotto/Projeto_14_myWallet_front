399e1b86455836fcb24765b1bba48947cfc814bf
Paulo <paulo_piotto@hotmail.com>
2021-10-24 01:09:13 -0300

    layout sign-in finalizado

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
+import { MainContainer, Logo, Input, Button, TextButton } from "../styles/style";
 
 export default function SignIn(){
     return (
-        <p>teste</p>
+        <MainContainer>
+            <Logo>My Wallet</Logo>
+            <Input type='text' placeholder='E-mail'></Input>
+            <Input type='text' placeholder='Senha'></Input>
+            <Button>Entrar</Button>
+            <TextButton>Primeira vez? Cadastre-se!</TextButton>
+        </MainContainer>
     );
 }
--- src/styles/style.js
+++ src/styles/style.js
@@ -2,8 +2,53 @@ import styled from "styled-components";
 
 const MainContainer = styled.div`
     width: 100vw;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     padding: 5%;
 `
+
+const Input = styled.input`
+    width: 100%;
+    height: 58px;
+    background-color: white;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    margin-bottom: 15px;
+    padding: 10px;
+`
+const Logo = styled.h1`
+    color: white;
+    font-family:'Saira Stencil One', cursive;
+    font-size: 32px;
+    margin-bottom: 30px;
+`
+const Button =  styled.button`
+    width: 100%;
+    height: 46px;
+    color: white;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    background-color: #A328D6;
+    font-size: 20px;
+    font-weight: 700;
+`
+
+const TextButton = styled.p`
+    color: white;
+    font-weight: 700;
+    font-size: 15px;
+    margin-top: 36px;
+`
+
+export{
+    MainContainer,
+    Input,
+    Logo,
+    Button,
+    TextButton,
+}
