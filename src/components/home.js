3f3c4d240b4365da047b34dcc40f27b2c394e176
Paulo <paulo_piotto@hotmail.com>
2021-11-29 00:07:08 -0300

    modifica readme e importações no componente home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import { MainContainer, Header } from "../styles/style";
-import styled from "styled-components";
-import { IoExitOutline, IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
+import { InfoTable, Buttons, Button, Add, Remove, Container } from "../styles/homeStyles";
+import { IoExitOutline } from "react-icons/io5";
 import { useHistory } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../contexts/userContext";
@@ -32,8 +32,6 @@ export default function Home(){
         history.push('/');
     }
 
-
-
     return (
         <MainContainer>
             <Header>
@@ -64,61 +62,3 @@ export default function Home(){
         </MainContainer>
     );
 }
-
-const InfoTable = styled.div`
-    width: 100%;
-    height: 68%;
-    background-color: white;
-    border-radius: 5px;
-    display: flex;
-    flex-direction: column;
-    justify-content: ${ ({transactions}) =>
-            !transactions.history[0] ? 'center' : 'space-between' };
-    align-items: center;
-    text-align: center;
-    color: ${ ({transactions}) =>
-            !transactions.history[0] ? '#868686' : 'black' };
-    padding: 15px;
-    /* position: relative; */
-    overflow-y: scroll;
-`
-
-const Buttons = styled.div`
-    width: 100%;
-    height: 20%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color: white;
-    margin-top: 15px;
-    font-size: 17px;
-    font-weight: 700;
-`
-
-const Button = styled.div`
-    width: 48%;
-    height: 90%;
-    background-color: #A328D6;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-    border-radius: 5px;
-    padding: 10px;
-
-    p{
-        width: 50%;
-    }
-`
-
-const Add = styled(IoAddCircleOutline)`
-    font-size: 25px;
-`
-
-const Remove = styled(IoRemoveCircleOutline)`
-    font-size: 25px;
-`
-
-const Container = styled.div`
-    width: 100%;
-`
